238653ec0d7b288b5fba49b3497de242a8cc9d3a
Álex Martín <115979684+ErXexi@users.noreply.github.com>
2022-10-17 10:21:52 +0200

    Delete Ejercicio2.java

--- Ejercicio2.java
+++
@@ -1,6 +0,0 @@
-/*2. Escribe un programa que dé los “buenos días”. */
-public class Ejercicio2 {
-    public static void main(String[] args) {
-        System.out.println("Buenos días!!!\n");
-    }
-}
