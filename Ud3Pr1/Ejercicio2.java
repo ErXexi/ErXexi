2f97b9f6d953a2ee3019b5639794974aa8c9ef0b
Álex Martín <115979684+ErXexi@users.noreply.github.com>
2022-10-17 10:23:08 +0200

    Add files via upload

---
+++ Ud3Pr1/Ejercicio2.java
@@ -0,0 +1,6 @@
+/*2. Escribe un programa que dé los “buenos días”. */
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Buenos días!!!\n");
+    }
+}
---
+++ Ud3Pr1/Ejercicio9.java
@@ -0,0 +1,29 @@
+/**9. Dado el siguiente programa, modifícalo para utilizar las variables que se indican. El
+tipo de dato elegido debe ser el de menos bits posibles que puedan representar el
+valor. Justifica tu elección.
+ */
+
+enum Semana { /*c) */
+    LUNES, MARTES, MIÉRCOLES, JUEVES, VIERNES
+}
+public class Ejercicio9 {
+    public static void main(String[ ] args) {
+        boolean casado = false; /*a) */
+
+        final int valor_max = 0; /*b) */
+
+        short dia_año = 0; /*d) */
+        
+        char sexo = '\u0000'; /*e) */
+
+        float ms = 0; /*f) */
+
+        int factura = 0; /*g) */
+
+        int poblacion_mundial = 0; /*h) */
+
+
+
+    }
+    }
+    
