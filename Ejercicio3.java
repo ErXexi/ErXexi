c37cd04ce066359ad20f545e933bb0a087930847
Álex Martín <115979684+ErXexi@users.noreply.github.com>
2022-10-17 10:19:56 +0200

    Ud3Pr1/

---
+++ Ejercicio3.java
@@ -0,0 +1,8 @@
+/*3. Escribe un programa que calcule y muestre el área de un cuadrado de lado igual a 5. */
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        int lado = 5, cuadrado = lado*lado;
+
+        System.out.println("El área de un cuadrado de lado "+ lado + " es " + cuadrado);
+    }
+}
